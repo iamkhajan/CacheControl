86c89bf97f3b09bbfd4d943ee205f97c2a5e7971
Khajan Pandey <khajanchandra.pandey@mx.bosch.com>
2018-07-31 10:02:46 -0500

    Initial Commit

---
+++ CacheDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CacheDemo
+//
+//  Created by khajan pandey on 7/31/18.
+//  Copyright © 2018 khajan pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
